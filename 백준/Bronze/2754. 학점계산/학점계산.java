ca21209b250a0571c4e70a3c1d66c7c1ca2dd15e
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2023-12-02 22:56:14 +0900

    [Bronze V] Title: 학점계산, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = br.readLine();
+            double score = 0;
+            switch (str) {
+                case "A+":
+                    score = 4.3;
+                    break;
+                case "A0":
+                    score = 4.0;
+                    break;
+                case "A-":
+                    score = 3.7;
+                    break;
+                case "B+":
+                    score = 3.3;
+                    break;
+                case "B0":
+                    score = 3.0;
+                    break;
+                case "B-":
+                    score = 2.7;
+                    break;
+                case "C+":
+                    score = 2.3;
+                    break;
+                case "C0":
+                    score = 2.0;
+                    break;
+                case "C-":
+                    score = 1.7;
+                    break;
+                case "D+":
+                    score = 1.3;
+                    break;
+                case "D0":
+                    score = 1.0;
+                    break;
+                case "D-":
+                    score = 0.7;
+                    break;
+                case "F":
+                    score = 0.0;
+                    break;
+                default:
+                    break;
+            }
+            System.out.println(score);
+        }
+    }
+}
