4a48c303bcdbf9e14807a1dbb16d299937d868a0
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2023-12-13 23:01:30 +0900

    [Bronze III] Title: 체스판 조각, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println((((N+1)/2)+1) * ((N/2)+1));
+        }
+    }
+}
