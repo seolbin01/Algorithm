a812d55d10c752589bc3a77794b1262b54eab224
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2023-12-14 21:36:59 +0900

    [Bronze V] Title: A+B - 7, Time: 168 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int T = Integer.parseInt(br.readLine());
+            for(int i=0;i<T;i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                System.out.println("Case #" + (i+1) + ": " + (A+B));
+            }
+        }
+    }
+}
