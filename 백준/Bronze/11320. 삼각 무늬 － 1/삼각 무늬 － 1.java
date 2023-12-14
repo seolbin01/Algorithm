cdf6ab22835e4647e2281033b18c73a919b622c7
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2023-12-14 21:57:37 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 128 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int T = Integer.parseInt(br.readLine());
+
+            for(int i=0;i<T;i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+
+                if(A%B == 0) System.out.println((int) Math.pow((A/B),2));
+            }
+        }
+    }
+}
