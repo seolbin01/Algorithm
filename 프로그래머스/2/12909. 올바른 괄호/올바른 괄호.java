5cecf255d578d9f25f2bf0bed42dcf8d9b9f766b
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2024-01-08 19:09:16 +0900

    [level 2] Title: 올바른 괄호, Time: 18.60 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,22 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
         
-        int open = 0;
-        int close = 0;
+        Stack<Integer> chk = new Stack<>();
+        
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '(') open++;
-            else close++;
-            
-            if(open < close){
-                answer = false;
-                break;
+            if(s.charAt(i) == '(') chk.push(0);
+            else if(s.charAt(i) == ')'){
+                if(chk.isEmpty()) {
+                    return false;
+                }
+                chk.pop();
             }
         }
-        if(open != close){
-            answer = false;
-        } 
+        
+        if(!chk.isEmpty()) answer = false;
         
         return answer;
     }
