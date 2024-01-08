c222f45110f8fda1779a35bcac74a8fbfb501f00
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2024-01-08 18:53:47 +0900

    [level 2] Title: 올바른 괄호, Time: 6.52 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        int open = 0;
+        int close = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(') open++;
+            else close++;
+            
+            if(open < close){
+                answer = false;
+                break;
+            }
+        }
+        if(open != close){
+            answer = false;
+        } 
+        
+        return answer;
+    }
+}
