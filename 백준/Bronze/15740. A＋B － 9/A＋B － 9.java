09446bf3970cd64dbe6efd6956cc58974bfab601
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2024-01-25 20:58:26 +0900

    [Bronze V] Title: A+B - 9, Time: 248 ms, Memory: 17412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            BigInteger a = new BigInteger(st.nextToken());
+            BigInteger b = new BigInteger(st.nextToken());
+            System.out.print(a.add(b));
+        }
+    }
+}
