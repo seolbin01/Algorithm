b680431559926209908dc4b4e7a1d1a969e4171a
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2024-02-08 20:56:45 +0900

    [Bronze V] Title: Who is in the middle?, Time: 116 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int[] arr = new int[3];
+            arr[0] = Integer.parseInt(br.readLine());
+            arr[1] = Integer.parseInt(br.readLine());
+            arr[2] = Integer.parseInt(br.readLine());
+            Arrays.sort(arr);
+            System.out.println(arr[1]);
+        }
+    }
+}
