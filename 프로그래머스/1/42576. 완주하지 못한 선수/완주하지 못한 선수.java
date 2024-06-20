355f7a0f699301e1ab172d4a4476ea6c53de69e5
seolbin01 <106576062+seolbin01@users.noreply.github.com>
2024-06-20 23:43:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.98 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,15 +3,19 @@ import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
         HashMap<String, Integer> map = new HashMap<>();
-        for(String part : participant) map.put(part, map.getOrDefault(part, 0)+1);
+        
+        for(String part : participant) map.put(part, map.getOrDefault(part, 0) + 1);
         for(String com : completion) map.put(com, map.get(com)-1);
-        for(String key : map.keySet()){
-            if(map.get(key) != 0){
-                answer = key;
+        
+        for(String name : map.keySet()) {
+            if(map.get(name) != 0) {
+                answer = name;
                 break;
             }
-        }
+        } 
+        
         return answer;
     }
 }
