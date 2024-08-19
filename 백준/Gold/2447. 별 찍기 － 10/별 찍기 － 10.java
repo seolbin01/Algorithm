5cc2f3cab848e758612a6d2c4d80ba676c65ee87
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2024-08-19 18:53:52 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 288 ms, Memory: 48244 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,67 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     static char[][] arr;
-
+    
     public static void main(String[] args) {
-
+        
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-
+         
             int n = Integer.parseInt(br.readLine());
-
+            
             arr = new char[n][n];
-
+            
             star(0, 0, n, false);
-
+            
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
                     sb.append(arr[i][j]);
                 }
                 sb.append('\n');
             }
             System.out.print(sb);
-
+            
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-
+    
     static void star(int x, int y, int n, boolean blank) {
-
-        // 공백칸일 경우
-        if (blank) {
-            for (int i = x; i < x + n; i++) {
-                for (int j = y; j < y + n; j++) {
+        
+        if(blank) {
+            for(int i=x;i<x+n;i++) {
+                for(int j=y;j<y+n;j++) {
                     arr[i][j] = ' ';
                 }
             }
             return;
         }
-
-        // 더이상 쪼갤 수 없는 블록일 때
-        if (n == 1) {
+        
+        if(n == 1) {
             arr[x][y] = '*';
             return;
         }
-
-		/*
-		   N=27 일 경우 한 블록의 사이즈는 9이고,
-		   N=9 일 경우 한 블록의 사이즈는 3이듯
-		   해당 블록의 한 칸을 담을 변수를 의미 size
-
-		   count는 별 출력 누적을 의미
-		 */
-
-        int size = n / 3;
+        
+        int size = n/3;
         int count = 0;
-        for (int i = x; i < x + n; i += size) {
-            for (int j = y; j < y + n; j += size) {
+        for(int i=x;i<x+n;i+=size) {
+            for(int j=y;j<y+n;j+=size) {
                 count++;
-                if (count == 5) { // 공백 칸일 경우
+                if(count == 5) {
                     star(i, j, size, true);
                 } else {
                     star(i, j, size, false);
