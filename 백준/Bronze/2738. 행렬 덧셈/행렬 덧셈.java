02825d932c8a41fe542ff18fcad957e6c157812a
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2024-08-21 15:01:00 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 152 ms, Memory: 15716 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] += Integer.parseInt(st.nextToken());
+                sb.append(arr[i][j]);
+                if (j < M - 1) {
+                    sb.append(" ");
+                }
+            }
+            if (i < N - 1) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.print(sb);
+
+        br.close();
+    }
+}
