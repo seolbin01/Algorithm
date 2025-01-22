e1c83ff30bdad2491bb86dccf5c22e536bd93960
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-01-23 02:33:04 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -8,14 +8,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] arr = br.readLine().toCharArray();
+        String str = br.readLine();
 
-        int length = arr.length;
+        int length = str.length();
 
         boolean chk = true;
 
         for (int i=0;i<length/2;i++) {
-            if (arr[i] != arr[length - 1 - i]) {
+            if (str.charAt(i) != str.charAt(length - 1 - i)) {
                 chk = false;
                 break;
             }
