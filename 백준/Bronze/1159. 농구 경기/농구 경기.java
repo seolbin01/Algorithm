411fc0976f4bbb55d3f4da33e41b277566e38e7e
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-01-23 02:52:06 +0900

    [Bronze II] Title: 농구 경기, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[26];
+
+        char first;
+
+        for (int i=0;i<num;i++) {
+            first = br.readLine().charAt(0);
+            arr[first - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0;i<26;i++) {
+            if (arr[i] >= 5) {
+                sb.append((char)('a' + i));
+            }
+        }
+
+        if (sb.length() > 0) {
+            System.out.println(sb);
+        } else {
+            System.out.println("PREDAJA");
+        }
+    }
+}
