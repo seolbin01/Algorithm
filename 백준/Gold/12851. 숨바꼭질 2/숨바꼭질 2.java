751db0a85b453ba8c21cc876d119668c0667bd8b
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-02-18 03:11:19 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 156 ms, Memory: 24584 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static int[] time = new int[100001];
+    static int[] count = new int[100001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        Arrays.fill(time, -1);
+        time[N] = 0;
+        count[N] = 1;
+
+        bfs();
+
+        System.out.println(time[K]);
+        System.out.println(count[K]);
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(N);
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+
+            int[] next = {current - 1, current + 1, current * 2};
+
+            for (int i=0;i<3;i++) {
+                if (next[i] < 0 || next[i] > 100000) continue;
+
+                if (time[next[i]] == -1) {
+                    time[next[i]] = time[current] + 1;
+                    count[next[i]] = count[current];
+                    q.offer(next[i]);
+                } else if (time[next[i]] == time[current] + 1) {
+                    count[next[i]] += count[current];
+                }
+            }
+        }
+    }
+}
