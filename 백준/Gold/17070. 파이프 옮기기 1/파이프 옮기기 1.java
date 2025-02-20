d9a353ce226361c4588f1a8d36cf0c980b3b940b
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-02-21 03:31:49 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 204 ms, Memory: 16144 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,15 +1,12 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
     static int[][] map;
-    static int[] pipeStatus = {0, 1, 2};  // 가로, 세로, 대각선
     static int count = 0;
 
     public static void main(String[] args) throws IOException {
