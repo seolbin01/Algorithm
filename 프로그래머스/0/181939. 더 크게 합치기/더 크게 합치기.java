71e1a1915096a6f7b6496dec9a19854ba657d9e7
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-03-14 21:01:09 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.37 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        answer = ab > ba ? ab : ba;
+        
+        return answer;
+    }
+}
