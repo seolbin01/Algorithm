749f220a41189ab42b3fae63799e5c71385a7702
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-03-14 20:45:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,9 +2,8 @@ class Solution {
     public String solution(String my_string) {
         StringBuilder answer = new StringBuilder();
         
-        for (int i=my_string.length()-1;i>=0;i--) {
-            answer.append(my_string.charAt(i));
-        }
+        answer.append(my_string);
+        answer.reverse();
         
         return answer.toString();
     }
