e282cd45b5ab82e3585c88e2e9b01680eb02a73e
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-03-14 19:32:07 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while (n > 0) {
+            if (n % 2 != 0) {
+                n--;
+            } else {
+                answer += n;
+                n -= 2;
+            }
+        }
+        
+        return answer;
+    }
+}
