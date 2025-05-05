b2d43ba282fa143a93d3be86a4d87c18b4c70f19
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-05-05 18:08:43 +0900

    [level 0] Title: n 번째 원소부터, Time: 7.75 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new LinkedList<>();
+        
+        for (int i=0;i<num_list.length;i++) {
+            if (i >= n-1) {
+                answer.add(num_list[i]);
+            }
+        }
+        
+        return answer.stream()
+                     .mapToInt(i->i)
+                     .toArray();
+    }
+}
