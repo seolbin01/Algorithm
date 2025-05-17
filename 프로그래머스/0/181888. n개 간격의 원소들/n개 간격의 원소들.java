32d0233e0adc4eb449662ff2d545242dfc2ad547
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-05-17 17:37:38 +0900

    [level 0] Title: n개 간격의 원소들, Time: 3.59 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new LinkedList<>();
+        
+        for (int i=0;i<num_list.length;i+=n) {
+            answer.add(num_list[i]);
+        }
+        
+        return answer.stream()
+                     .mapToInt(i->i)
+                     .toArray();
+    }
+}
