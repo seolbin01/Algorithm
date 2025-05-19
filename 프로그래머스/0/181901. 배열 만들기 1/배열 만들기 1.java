d242a65477cfe25da3726cb1cf0bd65c7933e93b
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-05-19 18:25:26 +0900

    [level 0] Title: 배열 만들기 1, Time: 3.60 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        List<Integer> answer = new LinkedList<>();
+        
+        for (int i=k;i<=n;i+=k) {
+            answer.add(i);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
