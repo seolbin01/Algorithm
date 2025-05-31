e5766e1bf32299096283acba625a0ac812ec00fa
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-05-31 13:57:23 +0900

    [level 0] Title: l로 만들기, Time: 1098.77 ms, Memory: 377 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for (int i=0;i<myString.length();i++) {
+            if (myString.charAt(i) - 'l' < 0) {
+                answer += "l";
+            } else {
+                answer += myString.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
