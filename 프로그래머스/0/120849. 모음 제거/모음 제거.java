d153c4f6a65d6f844d99aba16993c933660a36b8
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-06-22 18:13:24 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,13 +2,7 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        for (int i=0;i<my_string.length();i++) {
-            char c = my_string.charAt(i);
-            
-            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
-                answer += c;
-            }
-        }
+        answer = my_string.replaceAll("[aeiou]", "");
         
         return answer;
     }
