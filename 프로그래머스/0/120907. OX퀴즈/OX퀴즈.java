92939789bc8151716694842cb80fe2b523c4bc46
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-08-14 20:18:26 +0900

    [level 0] Title: OX퀴즈, Time: 0.21 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+
+        for (int i = 0; i < quiz.length; i++) {
+            String[] parts = quiz[i].split(" ");
+            int X = Integer.parseInt(parts[0]);
+            String op = parts[1];
+            int Y = Integer.parseInt(parts[2]);
+            int Z = Integer.parseInt(parts[4]);
+
+            int result = op.equals("+") ? X + Y : X - Y;
+
+            answer[i] = (result == Z) ? "O" : "X";
+        }
+
+        return answer;
+    }
+}
