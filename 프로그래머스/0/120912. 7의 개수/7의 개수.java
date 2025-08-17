d0861ad81ced45cf433dd326d95e1727ebefa13d
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-08-17 20:28:37 +0900

    [level 0] Title: 7의 개수, Time: 0.07 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for (int num : array) {
+            String str = String.valueOf(num);
+            
+            for (char c : str.toCharArray()) {
+                if (c == '7') {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
