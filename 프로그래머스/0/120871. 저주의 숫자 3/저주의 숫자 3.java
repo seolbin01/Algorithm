4473a736e9b1e6ceaf6c3add9208e7430d096920
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-08-26 21:37:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.34 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        int num = 0;
+
+        while (count < n) {
+            num++;
+            if (num % 3 == 0 || String.valueOf(num).contains("3")) {
+                continue; 
+            }
+            count++;
+        }
+
+        return num;
+    }
+}
