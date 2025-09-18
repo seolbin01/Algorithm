fdff640d7be0e6cb2e30aa9dcbda2bc5b5fb5c75
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-09-18 12:13:04 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int gcd = getGCD(n, m);
+        int lcm = n * m / gcd;
+        
+        return new int[]{gcd, lcm};
+    }
+    
+    // 유클리드 호제법
+    private int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
