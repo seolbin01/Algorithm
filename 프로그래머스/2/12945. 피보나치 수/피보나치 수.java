bd94ce2bbeea6e55a032b1972c8a8af656fd4e13
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-11-21 20:29:40 +0900

    [level 2] Title: 피보나치 수, Time: 2.70 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int mod = 1234567;
+        
+        int a = 0;
+        int b = 1;
+
+        for (int i=2;i<=n;i++) {
+            int temp = (a+b) % mod;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
