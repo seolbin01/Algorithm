319ce8595a44e038cc3b6c9864a25d92e7cbe8b8
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2025-11-28 01:08:29 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i=2;i<=n;i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+        System.out.println(fib[n]);
+    }
+}
