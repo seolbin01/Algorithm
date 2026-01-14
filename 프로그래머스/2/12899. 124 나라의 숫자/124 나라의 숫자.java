8ee966fa10c8d6c7a2dc7270772c68c1cbaf6595
Park Seolbin <106576062+seolbin01@users.noreply.github.com>
2026-01-15 05:30:54 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            int mod = n % 3;
+
+            if (mod == 1) {
+                sb.append('1');
+                n /= 3;
+            } else if (mod == 2) {
+                sb.append('2');
+                n /= 3;
+            } else {
+                sb.append('4');
+                n = n / 3 - 1;
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
